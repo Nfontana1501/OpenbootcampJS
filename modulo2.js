d7b3a6bce040aaf7d5d0052b9514f16ccc858d68
Nicolas Fontana <nicolas.93.fontana@gmail.com>
2022-06-24 19:43:58 -0300

    Ejercicio segundo modulo, curso javascript - Open Bootcamp

---
+++ modulo2.js
@@ -0,0 +1,14 @@
+const nombre = "Nicolas Fontana";
+const edad = 29;
+const desarrollador = false;
+const libro = {
+    titulo: "Angeles y demonios",
+    autor: "Dan Brown",
+    año: 2000,
+    url: "https://es.wikipedia.org/wiki/%C3%81ngeles_y_demonios_(novela)",
+}
+const fecha = new Date ("january 15 1993")
+
+const array = [nombre, edad, desarrollador, fecha, libro]
+
+console.log(array)
